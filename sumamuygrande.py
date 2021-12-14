0c15741329e768cb74cdd6338e81bfea5148a3ab
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2021-12-14 18:46:38 +0100

    ejercicio 3 incompleto

---
+++ sumamuygrande.py
@@ -0,0 +1,12 @@
+def aVeryBigSum(): 
+    filas_matriz = int(input("Introduce el numero de filas de tu matriz: "))
+    columnas_matriz = int(input("Introduce el numero de columnas de tu matriz: "))
+    tamano=filas_matriz*columnas_matriz
+    matriz = list(map(int,input("Introduce los numeros separados por un espacio").split()))
+    if len(matriz) != tamano:
+        print("El numero de elementos no coincide con el tamaño de la matriz")
+    else:
+        long = sum(matriz)
+        print(long)
+    
+aVeryBigSum()
